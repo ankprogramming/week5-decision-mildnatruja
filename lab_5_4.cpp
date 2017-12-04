4e6a39ccf0ba109180966edff1ff9134f9a4daae
Unknown <mildnatruja@gmail.com>
2017-12-04 18:04:30 +0700

    ข้อ4 เสร็จ

--- lab_5_4.cpp
+++ lab_5_4.cpp
@@ -10,8 +10,13 @@ int main() {
     cin >> menu;
 
     switch (menu) {
-        case 1: cout << "Call Program 1";
-    }
+        case 1: cout << "Call Program 1" << endl; break;
+        case 2: cout << "mild" << endl; break;
+        case 3: cout << "miw" << endl; break;
+        case 4: cout << "moo" << endl; break;
+        case 5: cout << "marr" << endl; break;
+        default : cout <<"error" ;
 
+    }
     return 0;
 }
